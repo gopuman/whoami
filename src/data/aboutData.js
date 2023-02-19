bafc50b922645fc43198c6556c2b4d74b2694c90
Gopal Nambiar <gopalnambiar2@gmail.com>
2023-02-19 14:45:17 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Gopal Nambiar. I'm a Developer and a master's in computer science student at UC Davis, CA.",
-    description2: "I have around two years of work experience at Oracle as a software Developer. I spend my leisure hours playing the guitar and working on interesting projects that would help me understand the practical side to the various concepts I study about. I love to learn and explore new arenas.",
+    description2: "I have around two years of work experience at Oracle as a software developer. I spend my leisure hours playing the guitar and working on interesting projects that would help me understand the practical side to the various concepts I study about. I love to learn and explore new arenas.",
     image: 2
 }
